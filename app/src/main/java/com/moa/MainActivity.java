4b20d18459c95ebb65c7dd29a1db92a403eced21
choiminseong <cms3136@gmail.com>
2019-08-20 15:14:35 +0900

    [ISSUE-1] webView 뒤로가기 제어
    - webView에서 뒤로가기 클릭시 이전 페이지로 이동할수 있도록 변경

--- app/src/main/java/com/moa/MainActivity.java
+++ app/src/main/java/com/moa/MainActivity.java
@@ -6,8 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import com.moa.handler.BackPressCloseHandler;
+
 public class MainActivity extends AppCompatActivity {
-    WebView webView;
+    private static final String MAIN_URL= "http://5racle.powerlinux.co.kr/admin";
+    private WebView webView;
+    private BackPressCloseHandler backPressCloseHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Intent intent = new Intent(this, LoadingActivity.class);
@@ -15,12 +20,15 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //--start of loading
+        backPressCloseHandler = new BackPressCloseHandler(this);
+        //--end of loading
         webView = findViewById(R.id.webView);
         webView.setWebViewClient(new MyWebClient());
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setHorizontalScrollBarEnabled(false);//가로스크롤없애기
         webView.setVerticalScrollBarEnabled(false);//세로스크롤없애기
-        webView.loadUrl("http://5racle.powerlinux.co.kr/admin");
+        webView.loadUrl(MAIN_URL);
     }
 
     class MyWebClient extends WebViewClient {
@@ -29,6 +37,17 @@ public class MainActivity extends AppCompatActivity {
             view.loadUrl(url);
 
             return true;
+        }
+    }
+
+    @Override// -- 뒤로가기 클릭시 엑티비티 종료 및 전환이 아닌 이전 페이지로 이동하게 한다.
+    public void onBackPressed() {
+        if(webView.getOriginalUrl().equalsIgnoreCase(MAIN_URL)){
+            backPressCloseHandler.onBackPressed();
+        }else if(webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
         }
     }
 }
